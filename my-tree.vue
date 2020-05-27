34fbeff3d1c8dcd22b3db7eef815d6fd5e8fd1e8
一个小小的前端攻城狮 <30406980+huangsheng-web@users.noreply.github.com>
2020-05-27 16:50:52 +0800

    Update my-tree.vue

--- my-tree.vue
+++ my-tree.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="tree-box">
+    <el-button size="mini" @click="setCheckKey">setCheckKey</el-button>
+    <el-button size="mini" @click="getCheckKey">getCheckKey</el-button>
+    <el-button size="mini" @click="gethalfCheckKey">gethalfCheckKey</el-button>
+    <el-button size="mini" @click="getCheckData">getCheckData</el-button>
+    <el-button size="mini" @click="gethalfCheckData">gethalfCheckData</el-button>
+    <el-button size="mini" @click="filterMethod">filter</el-button>
+    <el-button size="mini" @click="cancelFilter">cancel filter</el-button>
     <hs-tree
         :data="tableData"
         ref="myTree"
         :props="defaultProps"
-        @node-click="nodeTap"
         :filter-node-method="todoSome"
+        icon-class="el-icon-arrow-right"
         :mathDelayTime="500"
+        showCheckbox
+        :node-key="defaultProps.label"
+        :highlightConfig="highlightConfig"
     >
       <template v-slot="{ row }">
         <div v-if="row.isFather">{{row.label1}}</div>
@@ -24,19 +34,23 @@
         defaultProps: {
           label: 'label1',
           children: 'child'
+        },
+        highlightConfig: {
+          bg: '#E0F2FF',
+          color: '#0C70F8'
         }
       }
     },
     mounted() {
       function randomData(arr) {
-        let _r = 5;
+        let _r = 4;
         for (let i = 0; i < _r; i++) {
           let obj = {
             label1: '父级大幅度发送' + i,
             isFather: true,
             child: []
           }
-          let _sR = 40;
+          let _sR = 50;
           for (let j = 0; j < _sR; j++) {
             let sObj = {
               label1: '子级发斯蒂芬' + j,
@@ -60,10 +74,31 @@
 //        this.tableData.splice(2)
 //      }, 3000)
 //      setTimeout(() =>{
-//        this.$refs.myTree.filter('级')
+//        this.$refs.myTree.filter('0')
 //      }, 3000)
     },
     methods: {
+      filterMethod() {
+        this.$refs.myTree.filter('0')
+      },
+      cancelFilter() {
+        this.$refs.myTree.filter('')
+      },
+      setCheckKey() {
+        this.$refs.myTree.setCheckedKeys(['子级发斯蒂芬0', '孙级方式发顺丰1', '孙级方式发顺丰2']);
+      },
+      getCheckKey() {
+        console.log(this.$refs.myTree.getCheckedKeys())
+      },
+      gethalfCheckKey() {
+        console.log(this.$refs.myTree.getHalfCheckedKeys())
+      },
+      getCheckData() {
+        console.log(this.$refs.myTree.getCheckedNodes())
+      },
+      gethalfCheckData() {
+        console.log(this.$refs.myTree.getHalfCheckedNodes())
+      },
       nodeTap(ev, data) {
         console.log('dd', ev, data)
       },
@@ -78,10 +113,10 @@
     watch: {}
   }
 </script>
-<style scoped="scoped" lang="scss">
+<style lang="scss">
   .tree-box {
     width: 300px;
-    height: 600px;
+    height: 400px;
     border: 1px solid #999999;
     margin: 0 auto;
     svg {
@@ -89,5 +124,14 @@
       height: 100%;
       background: rgba(0, 0, 0, 0.2);
     }
+    /*.hs-tree-highlight-label {*/
+      /*color: #0C70F8;*/
+      /*background-color: #E0F2FF;*/
+      /*.hs-tree-arrow {*/
+        /*i {*/
+          /*color: #0C70F8;*/
+        /*}*/
+      /*}*/
+    /*}*/
   }
 </style>
